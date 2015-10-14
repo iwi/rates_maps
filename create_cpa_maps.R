b8406a90d001056d5f1546de7bfe4981b0823946
Annie Howard <annievic@hotmail.co.uk>
2015-10-14 12:24:45 +0100

    Initial commit

---
+++ create_cpa_maps.R
@@ -0,0 +1,75 @@
+#install the packages
+library(rgdal)
+library(mapproj)
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(plyr)
+if (!require(gpclib)) install.packages("gpclib", type="source")
+
+gpclibPermit()
+
+setwd("C:/Users/Annie/Documents/Work/Maps/cpa_maps/inputs")
+
+#read in laa to cpa mapping
+laa_cpa_mapping <- read.csv("laa_cpa.csv", header = TRUE)
+
+#read in LAA polygons
+laa_polygons <- readOGR("ctyua_ew_generalised_WGS84.json", "OGRGeoJSON")
+
+#merge by laa to add cpa column to polygon data
+laa_polygons_with_cpa_data <- merge(laa_polygons, laa_cpa_mapping,
+                                    by.x = "CTYUA13NM", by.y = "LAA",
+                                    all.x = TRUE, all.y = TRUE)
+
+# aggregate laa polygons by the 21 cpas (aggregate by cpa_code)
+cpa_polygons <- raster::aggregate(laa_polygons_with_cpa_data, "cpa_code")
+
+#fortify and merge to create the data frame ggplot will show on the map
+cpa_polygons@data$id <- rownames(cpa_polygons@data)
+cpa_polygons.points <- fortify(cpa_polygons, region = "id")
+cpa_polygons.df <- merge(cpa_polygons.points, cpa_polygons@data, sort=FALSE)
+
+# merge to add rates
+rates <- read.csv("cpa_rates.csv", header = TRUE)
+cpa_polygons.df <- merge(cpa_polygons.df, rates,
+                         by.x = "cpa_code", by.y = "cpa_code",
+                         all.x = TRUE, all.y = TRUE)
+
+# add quintiles field for discrete  scale mapping
+cpa_polygons.df$quintiles <- cut(cpa_polygons.df$rate,6)
+
+# discrete scale map
+ggplot(data = cpa_polygons.df, aes(x= long, y = lat, group = group, fill=quintiles)) +
+  geom_polygon() +
+  geom_path() +
+  ggtitle("Rates by CPA - in quintiles") +
+  scale_fill_brewer(palette="Blues")
+
+# continuous scale map
+ggplot(data = cpa_polygons.df, aes(x= long, y = lat, group = group, fill=rate)) +
+  geom_polygon() +
+  geom_path() +
+  ggtitle("Rates by CPA")
+
+###unfinished
+# add field for grouping by
+add.grouping.bins <- function(var){
+  if(var < 50){
+  "[0, 50)"} else {
+    if(var < 60){
+      "[50, 40)"} else {
+        if (var < 70){
+          "[60, 70)"} else {
+            "[70, 100)"
+          }
+      }
+  }
+}
+###
+
+#testing interactive maps
+library(googleVis)
+
+# add latlong var so package can be used
+cpa_polygons.df$lat_long <- paste(cpa_polygons.df$lat, ":", cpa_polygons.df$long, sep = "")
